79b5e5da0e67989c2b5be10f3740125e04143b96
Aoi020608 <62386689+Aoi020608@users.noreply.github.com>
2021-03-04 21:36:46 +0700

    add function

--- vulnerable.sol
+++ vulnerable.sol
@@ -4,6 +4,23 @@ import "./Library.sol";
 
 contract Vulnerable {
   
+  mapping (address => uint) balances;
   
+  function contribute() payable {
+    balances[msg.sender] += msg.value;
+  }
+  
+  function withdraw() {
+    if(balances[msg.sender] == 0) {
+      throw;
+    }
+    
+    balances[msg.sender] = 0;
+    msg.sender.call.value(balances[msg.sender])();
+  }
+  
+  function getFunds() returns (uint) {
+    return address(this).balance;
+  }
 
 }
